43c76c62e0485241a5db37c5f0ef985d226ba547
Arup Gorai <arup.gorai@digital.datamatics.com>
2021-12-04 21:04:56 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { Fragment } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <Fragment>
       <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+        <Text>Hello there from Expo..!!</Text>
+      </View>
+    </Fragment>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    borderWidth: 1,
+    borderColor: "red",
   },
 });
